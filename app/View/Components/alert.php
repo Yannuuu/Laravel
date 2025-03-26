cf7988fb0ba05dcb9386de8c2686e5ee3d47405c
Yanuu <yuhtukutoprak999@gmail.com>
2025-03-26 09:46:22 +0700

    [Pertemuan 3] Membuat Layout dan Component

--- app/View/Components/alert.php
+++ app/View/Components/alert.php
@@ -2,25 +2,20 @@
 
 namespace App\View\Components;
 
-use Closure;
-use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class alert extends Component
+class Alert extends Component
 {
-    /**
-     * Create a new component instance.
-     */
-    public function __construct()
+    public $type;
+    public $message;
+
+    public function __construct($type = 'primary', $message = 'Ini adalah alert!')
     {
-        //
+        $this->type = $type;
+        $this->message = $message;
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     */
-    public function render(): View|Closure|string
+    public function render()
     {
         return view('components.alert');
-    }
-}
+    }}
--- resources/views/web/homepage.blade.php
+++ resources/views/web/homepage.blade.php
@@ -1,3 +1,13 @@
 <x-layout> 
     <h3>Ini adalah halaman Homepage</h3> 
+    <x-card></x-card>
+        <!-- Alert Component (diletakkan di bawah card) -->
+        <x-alert type="primary" message="A simple primary alert—check it out!" />
+        <x-alert type="secondary" message="A simple secondary alert—check it out!" />
+        <x-alert type="success" message="A simple success alert—check it out!" />
+        <x-alert type="danger" message="A simple danger alert—check it out!" />
+        <x-alert type="warning" message="A simple warning alert—check it out!" />
+        <x-alert type="info" message="A simple info alert—check it out!" />
+        <x-alert type="light" message="A simple light alert—check it out!" />
+        <x-alert type="dark" message="A simple dark alert—check it out!" />
 </x-layout> 
